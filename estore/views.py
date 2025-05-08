98756e7a3845d23a3e22ab528afca8891fe467c1
Asif Iqbal <im.asif093@gmail.com>
2025-05-08 23:50:39 +0500

    all data flag is introduced to get data for choices

--- estore/views.py
+++ estore/views.py
@@ -5,6 +5,9 @@ from .models import Product, Category, Brand
 from .serializers import ProductSerializer, CategorySerializer, BrandSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from user.models import User
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework.response import Response
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -20,13 +23,48 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated, IsAdminUserRole]
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name='all_data',
+                description='Set to true to retrieve all data without pagination',
+                required=False,
+                type=bool
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        all_data = request.query_params.get('all_data', '').lower() == 'true'
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if all_data:
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+        
+        return super().list(request, *args, **kwargs)
+
 
 class BrandViewSet(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
     permission_classes = [IsAuthenticated, IsAdminUserRole]
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name='all_data',
+                description='Set to true to retrieve all data without pagination',
+                required=False,
+                type=bool
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        all_data = request.query_params.get('all_data', '').lower() == 'true'
+        queryset = self.filter_queryset(self.get_queryset())
 
-# class ClientViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.filter(role='user')
-#     serializer_class = ClientSerializer
-#     permission_classes = [IsAuthenticated, IsAdminUserRole]  
+        if all_data:
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+        
+        return super().list(request, *args, **kwargs)
+ 
